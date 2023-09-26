462f4686d6f4f56b49c5e210250816d1f8eb1334
shaharyarTernion <shaharyar.malik@ternionsolution.com>
2023-09-27 03:45:11 +0500

    schema updated

--- lambda/types.ts
+++ lambda/types.ts
@@ -7,9 +7,34 @@
 //   content: String;
 //   images?: String[];
 // };
+type Comment = {
+  commentId: String;
+  content: String;
+  authId: String;
+  postId: String;
+  likes: String;
+  date: String;
+  time: String;
+  authName: String;
+};
 
 export type PostInput = {
   id: String;
   title: String;
   content: String;
+  authId: String;
+  authorName: String;
+  postId: String;
+  time: GLfloat;
+  date: GLfloat;
+  images: String;
+  videos: String;
+  likes: String;
+  comments: Comment[];
+  links: String;
+  location: String;
+  privacy: String;
+  repost: String;
+  attachments: String;
+  views: String;
 };
